5a0eee56617eacbb47804e96f8ae758bc729613c
veeru <veeru@schoolforai.com>
2024-11-13 19:38:57 +0530

    Fixes for all tasks

--- frontend/src/pages/CedTasks.vue
+++ frontend/src/pages/CedTasks.vue
@@ -1,18 +1,29 @@
 <template>
     <div>
         <header class="sticky top-0 z-10 flex items-center justify-between border-b bg-white px-5 py-2.5">
-            <Breadcrumbs class="h-7" :items="[{ label: 'My Tasks', route: { name: 'MyTasks' } }]" />
-            <Button variant="solid" @click="showNewTaskDialog">
-                <template #prefix>
-                    <LucidePlus class="h-4 w-4" />
-                </template>
-                Add new
-            </Button>
+            <Breadcrumbs class="h-7" :items="[{ label: 'All Tasks', route: { name: 'CedAll' } }]" />
+            <div class="flex">
+                <Select class="w-44 pl-7 pr-7 mr-4" :options="[
+                    { label: 'Creation Date ASC', value: 'creation asc' },
+                    { label: 'Creation Date DESC', value: 'creation desc' }
+                ]" v-model="orderBy">
+                    <template #prefix>
+                        <LucideArrowDownUp class="w-4 text-gray-600" />
+                    </template>
+                </Select>
+                <Button variant="solid" @click="showNewTaskDialog">
+                    <template #prefix>
+                        <LucidePlus class="h-4 w-4" />
+                    </template>
+                    Add new
+                </Button>
+            </div>
+
         </header>
 
         <div class="mx-auto w-full max-w-4xl px-5">
             <div class="py-6">
-                <TaskList :listOptions="listOptions" :groupByStatus="true" />
+                <TaskList :listOptions="listOptions" :groupByStatus="false" />
                 <NewTaskDialog ref="newTaskDialog" />
             </div>
         </div>
@@ -27,6 +38,8 @@ let newTaskDialog = ref(null)
 
 let listOptions = computed(() => ({
     filters: {},
+    pageLength: 999
+    orderBy: 'creation desc'
 }))
 
 function showNewTaskDialog() {
