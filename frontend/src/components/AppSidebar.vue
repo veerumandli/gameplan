fc18c26f83a1aee693047a7170bc9352e1516be8
veeru <veeru@schoolforai.com>
2024-11-13 17:56:46 +0530

    add the All tasks page

--- frontend/src/components/AppSidebar.vue
+++ frontend/src/components/AppSidebar.vue
@@ -1,32 +1,22 @@
 <template>
   <div
     class="absolute right-0 z-10 h-full w-1 cursor-col-resize bg-surface-gray-4 opacity-0 transition-opacity hover:opacity-100"
-    :class="{ 'opacity-100': sidebarResizing }"
-    @mousedown="startResize"
-  />
-  <div
-    v-show="sidebarResizing"
+    :class="{ 'opacity-100': sidebarResizing }" @mousedown="startResize" />
+  <div v-show="sidebarResizing"
     class="fixed z-20 mt-3 -translate-x-[130%] rounded-md bg-surface-gray-6 px-2 py-1 text-base text-ink-white"
-    :style="{ left: sidebarWidth + 'px' }"
-  >
+    :style="{ left: sidebarWidth + 'px' }">
     {{ sidebarWidth }}
   </div>
 
-  <div
-    class="inline-flex h-full flex-1 flex-col overflow-auto border-r bg-surface-menu-bar pb-40"
-    :style="{ width: `${sidebarWidth}px` }"
-  >
+  <div class="inline-flex h-full flex-1 flex-col overflow-auto border-r bg-surface-menu-bar pb-40"
+    :style="{ width: `${sidebarWidth}px` }">
     <div class="flex flex-col px-2 py-2">
       <UserDropdown />
     </div>
     <div class="flex-1">
       <nav class="space-y-0.5 px-2">
-        <Links
-          :links="navigation"
-          class="flex items-center rounded px-2 py-1 text-ink-gray-8 transition"
-          active="bg-surface-selected shadow-sm"
-          inactive="hover:bg-surface-gray-2"
-        >
+        <Links :links="navigation" class="flex items-center rounded px-2 py-1 text-ink-gray-8 transition"
+          active="bg-surface-selected shadow-sm" inactive="hover:bg-surface-gray-2">
           <template v-slot="{ link }">
             <div class="flex w-full items-center space-x-2">
               <span class="grid h-5 w-6 place-items-center">
@@ -39,16 +29,11 @@
             </div>
           </template>
         </Links>
-        <button
-          v-if="$user().isNotGuest"
-          class="flex w-full items-center rounded px-2 py-1 text-ink-gray-8"
-          :class="[
-            /Search/.test($route.name)
-              ? 'bg-surface-selected shadow-sm'
-              : 'hover:bg-surface-gray-2',
-          ]"
-          @click="showCommandPalette"
-        >
+        <button v-if="$user().isNotGuest" class="flex w-full items-center rounded px-2 py-1 text-ink-gray-8" :class="[
+          /Search/.test($route.name)
+            ? 'bg-surface-selected shadow-sm'
+            : 'hover:bg-surface-gray-2',
+        ]" @click="showCommandPalette">
           <div class="flex w-full items-center">
             <span class="grid h-5 w-6 place-items-center">
               <LucideSearch class="h-4 w-4 text-ink-gray-7" />
@@ -64,59 +49,47 @@
       <div class="mt-6 flex items-center justify-between px-3">
         <h3 class="text-sm font-medium text-ink-gray-5">Teams</h3>
         <Button label="Create Team" variant="ghost" @click="showAddTeamDialog = true">
-          <template #icon><LucidePlus class="h-4 w-4" /></template>
+          <template #icon>
+            <LucidePlus class="h-4 w-4" />
+          </template>
         </Button>
       </div>
       <nav class="mt-1 space-y-0.5 px-2">
         <div v-for="team in activeTeams" :key="team.name">
           <Link :link="team" class="flex items-center rounded px-2 py-1 transition">
-            <button
-              @click.prevent="
-                () => {
-                  team.open = !team.open
-                }
-              "
-              class="mr-1.5 grid h-4 w-4 place-items-center rounded hover:bg-surface-gray-3"
-            >
-              <ChevronTriangle
-                class="h-3 w-3 text-ink-gray-4 transition duration-200"
-                :class="[team.open ? 'rotate-90' : 'rotate-0']"
-              />
-            </button>
-            <div class="flex w-full items-center">
-              <span class="flex h-5 w-5 items-center justify-center text-xl">
-                {{ team.icon }}
-              </span>
-              <span class="ml-2 text-sm">{{ team.title }}</span>
-              <LucideLock v-if="team.is_private" class="ml-2 h-3 w-3" />
-              <div class="ml-auto">
-                <Tooltip v-if="team.unread" :text="`${team.unread} unread posts`">
-                  <span class="text-xs text-ink-gray-5">{{ team.unread }}</span>
-                </Tooltip>
-              </div>
+          <button @click.prevent="() => {
+            team.open = !team.open
+          }
+            " class="mr-1.5 grid h-4 w-4 place-items-center rounded hover:bg-surface-gray-3">
+            <ChevronTriangle class="h-3 w-3 text-ink-gray-4 transition duration-200"
+              :class="[team.open ? 'rotate-90' : 'rotate-0']" />
+          </button>
+          <div class="flex w-full items-center">
+            <span class="flex h-5 w-5 items-center justify-center text-xl">
+              {{ team.icon }}
+            </span>
+            <span class="ml-2 text-sm">{{ team.title }}</span>
+            <LucideLock v-if="team.is_private" class="ml-2 h-3 w-3" />
+            <div class="ml-auto">
+              <Tooltip v-if="team.unread" :text="`${team.unread} unread posts`">
+                <span class="text-xs text-ink-gray-5">{{ team.unread }}</span>
+              </Tooltip>
             </div>
+          </div>
           </Link>
           <div class="mb-2 mt-0.5 space-y-0.5 pl-7" v-show="team.open">
-            <Link
-              :key="project.name"
-              v-for="project in teamProjects(team.name)"
-              :link="project"
+            <Link :key="project.name" v-for="project in teamProjects(team.name)" :link="project"
               :ref="($comp) => setProjectRef($comp, project)"
               class="flex h-7 items-center rounded-md px-2 text-ink-gray-8 transition"
-              active="bg-surface-selected shadow-sm"
-              inactive="hover:bg-surface-gray-2"
-            >
-              <template v-slot="{ link: project }">
-                <span class="inline-flex items-center space-x-2">
-                  <span class="text-sm">{{ project.title }}</span>
-                  <LucideLock v-if="project.is_private" class="h-3 w-3" />
-                </span>
-              </template>
+              active="bg-surface-selected shadow-sm" inactive="hover:bg-surface-gray-2">
+            <template v-slot="{ link: project }">
+              <span class="inline-flex items-center space-x-2">
+                <span class="text-sm">{{ project.title }}</span>
+                <LucideLock v-if="project.is_private" class="h-3 w-3" />
+              </span>
+            </template>
             </Link>
-            <div
-              class="flex h-7 items-center px-2 text-sm text-ink-gray-5"
-              v-if="teamProjects(team.name).length === 0"
-            >
+            <div class="flex h-7 items-center px-2 text-sm text-ink-gray-5" v-if="teamProjects(team.name).length === 0">
               No projects
             </div>
           </div>
@@ -126,20 +99,17 @@
         No teams
       </div>
     </div>
-    <AddTeamDialog
-      v-model:show="showAddTeamDialog"
-      @success="
-        (team) => {
-          showAddTeamDialog = false
-          $router.push({
-            name: 'TeamOverview',
-            params: { teamId: team.name },
-          })
-        }
-      "
-    />
+    <AddTeamDialog v-model:show="showAddTeamDialog" @success="(team) => {
+      showAddTeamDialog = false
+      $router.push({
+        name: 'TeamOverview',
+        params: { teamId: team.name },
+      })
+    }
+      " />
   </div>
 </template>
+
 <script>
 import { Tooltip } from 'frappe-ui'
 import Links from './Links.vue'
@@ -190,6 +160,15 @@ export default {
           route: {
             name: 'Discussions',
           },
+        },
+        {
+          name: 'All Tasks',
+          icon: LucideListTodo,
+          route: {
+            name: 'CedTasks',
+          },
+          isActive: /CedTasks/g.test(this.$route.name),
+          condition: () => this.$user().isNotGuest,
         },
         {
           name: 'My Tasks',
